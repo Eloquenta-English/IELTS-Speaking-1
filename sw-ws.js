b68eeecbc87358120ae299c81fcb40f6c6689288
William Gray Thomason <billiethomasonsound@gmail.com>
2026-07-17 16:53:02 +0200

    feat: Vehicle setup + dashboard for Digital Workshop

--- sw-ws.js
+++ sw-ws.js
@@ -1,4 +1,4 @@
-const CACHE = 'oil-change-v1';
-const ASSETS = ['./', './oil-change.html', './manifest-ws.json', './sw-ws.js'];
+const CACHE = 'workshop-v1';
+const ASSETS = ['./','./index-ws.html','./dashboard.html','./oil-change.html','./manifest-ws.json','./sw-ws.js'];
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
